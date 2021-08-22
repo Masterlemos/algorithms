7744f3b6b0edabca8681463402161513529c5ef5
Aldo Lemos <aldorlemos@gmail.com>
2021-08-22 17:26:01 -0300

    feat: fenwick tree

---
+++ DataStructures/FenwickTree/FenwickTree/FenwickTree/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace FenwickTreeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Fenwick Tree!");
+
+            int[] heightRisk = new int[] { 0, 10, 28, 3, 2, 7, 4, 6, -53 };
+
+            FenwickTree fenwickTree = new FenwickTree(9);
+
+            for (int i = 1; i < 9; i++)
+            {
+                fenwickTree.IncrementAtIndex(i, heightRisk[i]);
+            }
+
+            Console.WriteLine(fenwickTree.RangeSumQuery(1, 1));
+            Console.WriteLine(fenwickTree.RangeSumQuery(1, 2));
+            Console.WriteLine(fenwickTree.RangeSumQuery(1, 3));
+            Console.WriteLine(fenwickTree.RangeSumQuery(3, 8));
+            Console.WriteLine(fenwickTree.RangeSumQuery(1, 8));
+
+            fenwickTree.IncrementAtIndex(3, 10);
+            Console.WriteLine(fenwickTree.RangeSumQuery(1, 8));
+
+            fenwickTree.IncrementAtIndex(3, -10);
+            Console.WriteLine(fenwickTree.RangeSumQuery(1, 8));
+
+            Console.ReadKey();
+        }
+    }
+
+    public class FenwickTree
+    {
+        private const int Zero = 0;
+
+        private readonly int[] cumulativeTable;
+
+        public FenwickTree(int size)
+        {
+            cumulativeTable = new int[size];          
+        }
+
+        public int LeastSignificantOneBitOf(int number)
+        {
+            return (number & (-number));
+        }
+
+        public int RangeSumQuery(int endIndex)
+        {
+            int sum = 0;
+            for (; endIndex > 0; endIndex -= LeastSignificantOneBitOf(endIndex))
+            {
+                sum += cumulativeTable[endIndex];
+            }
+            return sum;
+        }
+
+        public int RangeSumQuery(int startIndex, int endIndex)
+        {
+            int rangeToSubtract = startIndex == 1 ? Zero : RangeSumQuery(startIndex - 1);
+            return RangeSumQuery(endIndex) - rangeToSubtract;
+        }
+
+        public void IncrementAtIndex(int index, int value)
+        {
+            for (; index < cumulativeTable.Length; index += LeastSignificantOneBitOf(index))
+            {
+                cumulativeTable[index] += value;
+            }
+        }
+    }
+}
---
+++ DataStructures/FenwickTree/FenwickTree/UnitTests/RangeSumQueryTest.cs
@@ -0,0 +1,84 @@
+﻿using FenwickTreeApp;
+using Xunit;
+
+namespace UnitTests
+{
+    public class RangeSumQuery
+    {
+        [Theory]
+        [InlineData(1, 1, 10)]
+        [InlineData(2, 2, 28)]
+        [InlineData(3, 3, 3)]
+        [InlineData(4, 4, 2)]
+        [InlineData(5, 5, 7)]
+        [InlineData(6, 6, 4)]
+        [InlineData(7, 7, 6)]
+        [InlineData(8, 8, -53)]
+        public void ShouldCalculateRangeSumQueryWhenIsSingleIndex(int heightStart, int heightEnd, int rangeRisk)
+        {
+            //Arrange
+            int[] heightRisk = new int[] { 0, 10, 28, 3, 2, 7, 4, 6, -53 };
+
+            FenwickTree fenwickTree = new FenwickTree(9);
+
+            for (int i = 1; i < 9; i++)
+            {
+                fenwickTree.IncrementAtIndex(i, heightRisk[i]);
+            }
+
+            //Act
+            int result = fenwickTree.RangeSumQuery(heightStart, heightEnd);
+
+            //Assert
+            Assert.Equal(result, rangeRisk);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 38)]
+        [InlineData(1, 3, 41)]
+        [InlineData(6, 8, -43)]
+        [InlineData(3, 7, 22)]
+        [InlineData(1, 8, 7)]
+        public void ShouldCalculateRangeSumQuery(int heightStart, int heightEnd, int rangeRisk)
+        {
+            //Arrange
+            int[] heightRisk = new int[] { 0, 10, 28, 3, 2, 7, 4, 6, -53 };
+
+            FenwickTree fenwickTree = new FenwickTree(9);
+
+            for (int i = 1; i < 9; i++)
+            {
+                fenwickTree.IncrementAtIndex(i, heightRisk[i]);
+            }
+
+            //Act
+            int result = fenwickTree.RangeSumQuery(heightStart, heightEnd);
+
+            //Assert
+            Assert.Equal(result, rangeRisk);
+        }
+
+        [Theory]
+        [InlineData(1, 8, -3)]
+        [InlineData(1, 1, 0)]
+        public void ShouldCalculateRangeSumQueryWithNegativeIncrement(int heightStart, int heightEnd, int rangeRisk)
+        {
+            //Arrange
+            int[] heightRisk = new int[] { 0, 10, 28, 3, 2, 7, 4, 6, -53 };
+
+            FenwickTree fenwickTree = new FenwickTree(9);
+
+            for (int i = 1; i < 9; i++)
+            {
+                fenwickTree.IncrementAtIndex(i, heightRisk[i]);
+            }
+
+            //Act
+            fenwickTree.IncrementAtIndex(1, -10);
+            int result = fenwickTree.RangeSumQuery(heightStart, heightEnd);
+
+            //Assert
+            Assert.Equal(result, rangeRisk);
+        }
+    }
+}
